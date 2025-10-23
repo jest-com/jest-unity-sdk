17fd0b4d30f7c759dbd5eed29e8da364ffdeb9c9
UmairJaved <umair.javed66@gmail.com>
2025-10-23 10:17:30 +0500

    Added namespace in demo project

--- UnityDemo/Assets/Jest/Scripts/Controllers/EventsController.cs
+++ UnityDemo/Assets/Jest/Scripts/Controllers/EventsController.cs
@@ -3,38 +3,43 @@ using com.jest.sdk;
 using TMPro;
 using UnityEngine;
 
-public class EventsController : MonoBehaviour
+
+namespace com.jest.demo
 {
-
-    [SerializeField] private TMP_InputField m_eventNameTextInput;
-    [SerializeField] private TMP_InputField m_eventParamsTextInput;
-
-
-    public void SendEvent()
+    public class EventsController : MonoBehaviour
     {
+        [SerializeField] private TMP_InputField m_eventNameTextInput;
+        [SerializeField] private TMP_InputField m_eventParamsTextInput;
 
-        string eventName = m_eventNameTextInput.text;
-        string eventParam = m_eventParamsTextInput.text;
-        if (string.IsNullOrEmpty(eventName))
+        public void SendEvent()
         {
-            UIManager.Instance.m_toastUI.ShowToast("Event name is empty");
-            return;
-        }
-        Dictionary<string, object> properties;
-        try
-        {
-            properties = Convert.FromString<Dictionary<string, object>>(eventParam);
-        }
-        catch (System.Exception e)
-        {
-            Debug.LogError(e);
-            properties = new Dictionary<string, object>();
-        }
-        JestSDK.Instance?.Analytics.CaptureEvent(eventName, properties);
-        UIManager.Instance.m_toastUI.ShowToast("Event sent successfully.");
-        m_eventNameTextInput.text = default;
-        m_eventParamsTextInput.text = "{\"score\":10, \"coins\": 100}";
+            string eventName = m_eventNameTextInput.text;
+            string eventParam = m_eventParamsTextInput.text;
 
-        GameManager.Instance.TriggerGameStateChangeEvent();
+            if (string.IsNullOrEmpty(eventName))
+            {
+                UIManager.Instance.m_toastUI.ShowToast("Event name is empty");
+                return;
+            }
+
+            Dictionary<string, object> properties;
+            try
+            {
+                properties = Convert.FromString<Dictionary<string, object>>(eventParam);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(e);
+                properties = new Dictionary<string, object>();
+            }
+
+            JestSDK.Instance?.Analytics.CaptureEvent(eventName, properties);
+            UIManager.Instance.m_toastUI.ShowToast("Event sent successfully.");
+
+            m_eventNameTextInput.text = string.Empty;
+            m_eventParamsTextInput.text = "{\"score\":10, \"coins\":100}";
+
+            GameManager.Instance.TriggerGameStateChangeEvent();
+        }
     }
 }
